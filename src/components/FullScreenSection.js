1fea3f635aa0cccbb690f91404bf8e2c0c6cebcb
Jonatan Fontebasso <jonfonte@MacBook-Air-de-Jonatan.local>
2026-05-27 23:21:09 +0100

    full

--- src/components/FullScreenSection.js
+++ src/components/FullScreenSection.js
@@ -1,18 +1,36 @@
-import * as React from "react"; 
-import { VStack } from "@chakra-ui/react"; 
+import * as React from "react";
+import { VStack } from "@chakra-ui/react";
 
+const FullScreenSection = ({
+  children,
+  isDarkBackground,
+  backgroundColor,
+  ...boxProps
+}) => {
+  return (
+    <VStack
+      width="100%"
+      minHeight="100vh"
+      position="relative"
+      backgroundColor={backgroundColor}
+      color={isDarkBackground ? "white" : "black"}
+      spacing={0}
+      m={0}
+      p={0}
+    >
+      {/* CONTENIDO CENTRADO INTERNO */}
+      <VStack
+        width="100%"
+        maxW="1280px"
+        minH="100vh"
+        justifyContent="center"
+        alignItems="center"
+        {...boxProps}
+      >
+        {children}
+      </VStack>
+    </VStack>
+  );
+};
 
-const FullScreenSection = ({ children, isDarkBackground, ...boxProps }) => { 
- return ( 
-   <VStack 
-     backgroundColor={boxProps.backgroundColor} 
-     color={isDarkBackground ? "white" : "black"} 
-   > 
-     <VStack maxWidth="1280px" minHeight="100vh" {...boxProps}> 
-       {children} 
-     </VStack> 
-   </VStack> 
- ); 
-}; 
- 
 export default FullScreenSection;
