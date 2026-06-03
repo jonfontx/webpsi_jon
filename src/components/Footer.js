cb637ff2cac775c83507880914a9c1f5c2a647dd
Jonatan Fontebasso <jonfonte@MacBook-Air-de-Jonatan.local>
2026-06-03 19:54:23 +0100

    foot

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Box, Flex, Link, Stack, Text } from "@chakra-ui/react";
+import { Link as RouterLink } from "react-router-dom";
 
 const Footer = () => {
   return (
@@ -16,18 +17,29 @@ const Footer = () => {
           direction="column"
           gap={2}
         >
-          <Text fontSize="sm" textAlign="center">
+          {/* NOMBRE CLICABLE */}
+          <Text
+            fontSize="sm"
+            textAlign="center"
+            as={RouterLink}
+            to="/"
+            cursor="pointer"
+            _hover={{ opacity: 0.8 }}
+          >
             Psicólogo Jonatan Fontebasso • © 2026 • Todos los derechos reservados
           </Text>
 
+          {/* LINKS */}
           <Stack direction="row" spacing={4} fontSize="sm" opacity={0.8}>
-            <Link href="/aviso-legal" _hover={{ opacity: 1 }}>
+            <Link as={RouterLink} to="/aviso-legal" _hover={{ opacity: 1 }}>
               Aviso legal
             </Link>
-            <Link href="/privacidad" _hover={{ opacity: 1 }}>
+
+            <Link as={RouterLink} to="/privacidad" _hover={{ opacity: 1 }}>
               Privacidad
             </Link>
-            <Link href="/cookies" _hover={{ opacity: 1 }}>
+
+            <Link as={RouterLink} to="/cookies" _hover={{ opacity: 1 }}>
               Cookies
             </Link>
           </Stack>
