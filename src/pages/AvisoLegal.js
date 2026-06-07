50dac693864c8c490dc007bce240ff325807eaba
Jonatan Fontebasso <jonfonte@MacBook-Air-de-Jonatan.local>
2026-06-07 17:52:03 +0100

    legales

--- src/pages/AvisoLegal.js
+++ src/pages/AvisoLegal.js
@@ -5,7 +5,32 @@ export default function AvisoLegal() {
     <Box maxW="800px" mx="auto" py={10} px={6}>
       <Heading mb={6}>Aviso legal</Heading>
       <Text fontSize="sm">
-        Aquí va tu texto legal...
+        AVISO LEGAL
+En cumplimiento con el deber de información recogido en artículo 10 de la Ley 34/2002, de 11 de julio, de Servicios de la Sociedad de la Información y del Comercio Electrónico (LSSICE), el propietario de la web , le informa de lo siguiente:
+
+Denominación social: Jonatan Luis Fontebasso
+NIF: Y8989108D
+Domicilio: C/ Hespérides, 5, 38630 - Arona (Santa Cruz De Tenerife)
+Autorización administrativa: Numero de Registro Sanitario: 9458
+Colegio profesional: Colegio Oficial de Psicología de Santa Cruz de Tenerife
+Colegiado: T-04523
+Título académico: Master Universitario en Psicología General Sanitaria
+Estado de expedición: España
+Normas deontológicas: El ejercicio profesional se encuentra sujeto a la normativa sanitaria y de colegios profesionales aplicable en España, así como al Código Deontológico de la profesión de psicólogo.
+Enlace consulta deontológicas: Las normas profesionales aplicables pueden consultarse en el Boletín Oficial del Estado (BOE), en la página web del Boletín Oficial del Estado (BOE) y en la página web del Consejo General de la Psicología de España.
+Con los límites establecidos en la ley, Jonatan Luis Fontebasso no asume ninguna responsabilidad derivada de la falta de veracidad, integridad, actualización y precisión de los datos o informaciones que contienen sus páginas web.
+
+Los contenidos e información no vinculan a Jonatan Luis Fontebasso ni constituyen opiniones, consejos o asesoramiento legal de ningún tipo pues se trata meramente de un servicio ofrecido con carácter informativo y divulgativo.
+
+Las páginas de Internet de Jonatan Luis Fontebasso pueden contener enlaces (links) a otras páginas de terceras partes que Jonatan Luis Fontebasso no puede controlar. Por lo tanto, Jonatan Luis Fontebasso no puede asumir responsabilidades por el contenido que pueda aparecer en páginas de terceros.
+
+Los textos, imágenes, sonidos, animaciones, software y el resto de contenidos incluidos en este website son propiedad exclusiva de Jonatan Luis Fontebasso o sus licenciantes. Cualquier acto de transmisión, distribución, cesión, reproducción, almacenamiento o comunicación pública total o parcial, deberá contar con el consentimiento expreso de Jonatan Luis Fontebasso .
+
+Asimismo, para acceder a algunos de los servicios que Jonatan Luis Fontebasso ofrece a través del sitio web, deberá proporcionar algunos datos de carácter personal. En cumplimiento de lo establecido en el Reglamento (UE) 2016/679 del Parlamento Europeo y del Consejo, de 27 de abril de 2016, relativo a la protección de las personas físicas en lo que respecta al tratamiento de datos personales y a la libre circulación de estos datos le informamos que, mediante la cumplimentación de los presentes formularios, sus datos personales quedarán incorporados y serán tratados en los ficheros de Jonatan Luis Fontebasso con el fin de poderle prestar y ofrecer nuestros servicios así como para informarle de las mejoras del sitio Web.
+
+Le informamos también de que tendrá la posibilidad en todo momento de ejercer los derechos de acceso, rectificación, cancelación, oposición, limitación y portabilidad de sus datos de carácter personal, de manera gratuita mediante email a: jlfontebasso@icloud.com o en la dirección: C/ Hespérides, 5, Puerta 62, 38630 - Arona (Santa Cruz De Tenerife).
+
+REV: 20.3009
       </Text>
     </Box>
   );
--- src/pages/Privacidad.js
+++ src/pages/Privacidad.js
@@ -1,3 +1,100 @@
 export default function Privacidad() {
-  return <div>Política de privacidad</div>;
+  return <div>Protección de datos de carácter personal según el RGPD
+
+    ß
+Jonatan Luis Fontebasso , en aplicación de la normativa vigente en materia de protección de datos de carácter personal, informa que los datos personales que se recogen a través de los formularios del Sitio web: jonatanfontebasso.com, se incluyen en los ficheros automatizados específicos de usuarios de los servicios de Jonatan Luis Fontebasso
+
+La recogida y tratamiento automatizado de los datos de carácter personal tiene como finalidad el mantenimiento de la relación comercial y el desempeño de tareas de información, formación, asesoramiento y otras actividades propias de Jonatan Luis Fontebasso
+
+Estos datos únicamente serán cedidos a aquellas entidades que sean necesarias con el único objetivo de dar cumplimiento a la finalidad anteriormente expuesta.
+
+Jonatan Luis Fontebasso adopta las medidas necesarias para garantizar la seguridad, integridad y confidencialidad de los datos conforme a lo dispuesto en el Reglamento (UE) 2016/679 del Parlamento Europeo y del Consejo, de 27 de abril de 2016, relativo a la protección de las personas físicas en lo que respecta al tratamiento de datos personales y a la libre circulación de los mismos, y derogando la antigua LOPD, la nueva Ley Orgánica 3/2018, de 5 diciembre, de Protección de Datos y Garantía de los Derechos Digitales (LOPDGDD).
+
+El usuario podrá en cualquier momento ejercitar los derechos de acceso, oposición, rectificación, cancelación, limitación y portabilidad reconocidos en el citado Reglamento (UE). El ejercicio de estos derechos puede realizarlo el propio usuario a través de email a: jlfontebasso@icloud.com o en la dirección: C/ Hespérides, 5, Puerta 62, C.P. 38630 - Arona (Santa Cruz De Tenerife).
+
+El usuario manifiesta que todos los datos facilitados por él son ciertos y correctos, y se compromete a mantenerlos actualizados, comunicando los cambios a Jonatan Luis Fontebasso
+
+Finalidad del tratamiento de los datos personales:
+
+¿Con qué finalidad trataremos tus datos personales?
+
+En Jonatan Luis Fontebasso , trataremos tus datos personales recabados a través del Sitio Web: jonatanfontebasso.com, con las siguientes finalidades:
+
+Con propósitos de seguridad o prevención de fraude.
+Ejecutar un contrato suscrito a distancia con el usuario.
+Prestar los servicios contratados por el usuario.
+Te recordamos que puedes oponerte al envío de comunicaciones comerciales por cualquier vía y en cualquier momento, remitiendo un correo electrónico a la dirección anteriormente indicada.
+
+Los campos de dichos registros son de cumplimentación obligatoria, siendo imposible realizar las finalidades expresadas si no se aportan esos datos.
+
+¿Por cuánto tiempo se conservan los datos personales recabados?
+
+Los datos personales proporcionados se conservarán mientras se mantenga la relación comercial o no solicites su supresión y durante el plazo por el cuál pudieran derivarse responsabilidades legales por los servicios prestados.
+
+Legitimación:
+
+El tratamiento de tus datos se realiza con las siguientes bases jurídicas que legitiman el mismo:
+
+La solicitud de información y/o la contratación de los servicios de Jonatan Luis Fontebasso , cuyos términos y condiciones se pondrán a tu disposición en todo caso, de forma previa a una eventual contratación.
+El consentimiento libre, específico, informado e inequívoco, en tanto que te informamos poniendo a tu disposición la presente política de privacidad, que tras la lectura de la misma, en caso de estar conforme, puedes aceptar mediante una declaración o una clara acción afirmativa, como el marcado de una casilla dispuesta al efecto.
+En caso de que no nos facilites tus datos o lo hagas de forma errónea o incompleta, no podremos atender tu solicitud, resultando del todo imposible proporcionarte la información solicitada o llevar a cabo la contratación de los servicios.
+
+Destinatarios:
+
+Los datos no se comunicarán a ningún tercero ajeno a Jonatan Luis Fontebasso , salvo obligación legal.
+
+Se han contratado servicios de hosting ubicados fuera del Espacio Económico Europeo, en EEUU, considerado un destino seguro para transferencias internacionales de datos, en virtud del Privacy Shield (Escudo de Privacidad).
+
+Datos recopilados por usuarios de los servicios
+
+En los casos en que el usuario incluya ficheros con datos de carácter personal en los servidores de alojamiento compartido, Jonatan Luis Fontebasso no se hace responsable del incumplimiento por parte del usuario del RGPD.
+
+Derechos propiedad intelectual jonatanfontebasso.com
+
+Jonatan Luis Fontebasso es titular de todos los derechos de autor, propiedad intelectual, industrial, "know how" y cuantos otros derechos guardan relación con los contenidos del sitio web jonatanfontebasso.com y los servicios ofertados en el mismo, así como de los programas necesarios para su implementación y la información relacionada.
+
+No se permite la reproducción, publicación y/o uso no estrictamente privado de los contenidos, totales o parciales, del sitio web jonatanfontebasso.com sin el consentimiento previo y por escrito.
+
+Propiedad intelectual del software
+
+El usuario debe respetar los programas de terceros puestos a su disposición por Jonatan Luis Fontebasso , aún siendo gratuitos y/o de disposición pública.
+
+Jonatan Luis Fontebasso dispone de los derechos de explotación y propiedad intelectual necesarios del software.
+
+El usuario no adquiere derecho alguno o licencia por el servicio contratado, sobre el software necesario para la prestación del servicio, ni tampoco sobre la información técnica de seguimiento del servicio, excepción hecha de los derechos y licencias necesarios para el cumplimiento de los servicios contratados y únicamente durante la duración de los mismos.
+
+Para toda actuación que exceda del cumplimiento del contrato, el usuario necesitará autorización por escrito por parte de Jonatan Luis Fontebasso , quedando prohibido al usuario acceder, modificar, visualizar la configuración, estructura y ficheros de los servidores propiedad de Jonatan Luis Fontebasso , asumiendo la responsabilidad civil y penal derivada de cualquier incidencia que se pudiera producir en los servidores y sistemas de seguridad como consecuencia directa de una actuación negligente o maliciosa por su parte.
+
+Propiedad intelectual de los contenidos alojados
+
+Se prohíbe el uso contrario a la legislación sobre propiedad intelectual de los servicios prestados por Jonatan Luis Fontebasso y, en particular de:
+
+La utilización que resulte contraria a las leyes españolas o que infrinja los derechos de terceros.
+La publicación o la transmisión de cualquier contenido que, a juicio de Jonatan Luis Fontebasso , resulte violento, obsceno, abusivo, ilegal, racial, xenófobo o difamatorio.
+Los cracks, números de serie de programas o cualquier otro contenido que vulnere derechos de la propiedad intelectual de terceros.
+La recogida y/o utilización de datos personales de otros usuarios sin su consentimiento expreso o contraviniendo lo dispuesto en el Reglamento (UE) 2016/679 del Parlamento Europeo y del Consejo, de 27 de abril de 2016, relativo a la protección de las personas físicas en lo que respecta al tratamiento de datos personales y a la libre circulación de los mismos.
+La utilización del servidor de correo del dominio y de las direcciones de correo electrónico para el envío de correo masivo no deseado.
+El usuario tiene toda la responsabilidad sobre el contenido de su web, la información transmitida y almacenada, los enlaces de hipertexto, las reivindicaciones de terceros y las acciones legales en referencia a propiedad intelectual, derechos de terceros y protección de menores.
+
+El usuario es responsable respecto a las leyes y reglamentos en vigor y las reglas que tienen que ver con el funcionamiento del servicio online, comercio electrónico, derechos de autor, mantenimiento del orden público, así como principios universales de uso de Internet.
+
+El usuario indemnizará a Jonatan Luis Fontebasso por los gastos que generara la imputación de Jonatan Luis Fontebasso en alguna causa cuya responsabilidad fuera atribuible al usuario, incluidos honorarios y gastos de defensa jurídica, incluso en el caso de una decisión judicial no definitiva.
+
+Protección de la información alojada
+
+Jonatan Luis Fontebasso realiza copias de seguridad de los contenidos alojados en sus servidores, sin embargo no se responsabiliza de la pérdida o el borrado accidental de los datos por parte de los usuarios. De igual manera, no garantiza la reposición total de los datos borrados por los usuarios, ya que los citados datos podrían haber sido suprimidos y/o modificados durante el periodo del tiempo transcurrido desde la última copia de seguridad.
+
+Los servicios ofertados, excepto los servicios específicos de backup, no incluyen la reposición de los contenidos conservados en las copias de seguridad realizadas por Jonatan Luis Fontebasso , cuando esta pérdida sea imputable al usuario; en este caso, se determinará una tarifa acorde a la complejidad y volumen de la recuperación, siempre previa aceptación del usuario.
+
+La reposición de datos borrados sólo está incluida en el precio del servicio cuando la pérdida del contenido sea debida a causas atribuibles a Jonatan Luis Fontebasso .
+
+Comunicaciones comerciales
+
+En aplicación de la LSSI. Jonatan Luis Fontebasso no enviará comunicaciones publicitarias o promocionales por correo electrónico u otro medio de comunicación electrónica equivalente que previamente no hubieran sido solicitadas o expresamente autorizadas por los destinatarios de las mismas.
+
+En el caso de usuarios con los que exista una relación contractual previa, Jonatan Luis Fontebasso sí está autorizado al envío de comunicaciones comerciales referentes a productos o servicios de Jonatan Luis Fontebasso que sean similares a los que inicialmente fueron objeto de contratación con el cliente.
+
+En todo caso, el usuario, tras acreditar su identidad, podrá solicitar que no se le haga llegar más información comercial a través de los canales de Atención al Cliente.
+
+REV: 20.3009</div>;
 }
